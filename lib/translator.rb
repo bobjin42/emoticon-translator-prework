40eec55aa08a10966ce79b0eaba1c0519562fd4a
Yicheng Jin <bobjin42@gmail.com>
2018-06-26 19:14:36 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,17 +1,17 @@
 # require modules here
   require 'yaml'
   emoticons = YAML.load_file('./lib/emoticons.yml')
-  
-def load_library(file_path)
-  # code goes here
-  require 'yaml'
-  emoticons = YAML.load_file(file_path)
-  new_hash = {}
-  new_hash[:get_meaning] = {}
-  new_hash[:get_emoticon] = {}
+  emoticons
+# def load_library(file_path)
+#   # code goes here
+#   require 'yaml'
+#   emoticons = YAML.load_file(file_path)
+#   new_hash = {}
+#   new_hash[:get_meaning] = {}
+#   new_hash[:get_emoticon] = {}
   
     
-end
+# end
 
 # def get_japanese_emoticon
 #   # code goes here
