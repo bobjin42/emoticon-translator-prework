fe15c1abf8593d7c6d68cef018af220b1005a292
Yicheng Jin <bobjin42@gmail.com>
2018-06-26 19:30:46 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,10 +9,11 @@ def load_library(file_path)
   new_hash = {}
   emoticons.each{|word, emoticon|
   new_hash[:get_meaning] = {}
-  new_hash[:get_meaning][word] = emoticon
+  new_hash[:get_meaning][emoticon[1]] = word
   new_hash[:get_emoticon] = {}
+  new_hash[:get_emoticon][emoticon[0]] = emoticon[1]
   }
-    
+  new_hash
 end
 
 def get_japanese_emoticon
